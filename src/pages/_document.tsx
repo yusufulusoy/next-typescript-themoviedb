45635349cc2f30a99deea9433a0f399289689a7d
Yusuf Ulusoy <info@yusufulusoy.com.tr>
2022-02-26 13:02:19 +0300

    create custom `_document.tsx` for Next

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import type { DocumentContext } from "next/document";
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await NextDocument.getInitialProps(ctx);
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang="en" className="dark">
+        <Head />
+        <body className="antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
