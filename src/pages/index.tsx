fdad1f860f4d35d2a388e08c9dc4993d970ee994
Yusuf Ulusoy <info@yusufulusoy.com.tr>
2022-03-04 21:45:06 +0300

    add `upcoming` section to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-import { Brand, Search } from "@components";
+import { Brand, MovieCard, Search } from "@components";
 
-const Home: NextPage = () => {
+type Props = {
+  upcomings: ResponsePaginationType & { results: MovieSummaryType[] };
+};
+
+const Home: NextPage<Props> = ({ upcomings }) => {
   return (
     <div>
       <Head>
@@ -23,9 +27,31 @@ const Home: NextPage = () => {
             <Search />
           </div>
         </section>
+        <section className="container mx-auto my-5">
+          <h2 className="font-bold tracking-widest uppercase mb-2">
+            Upcoming Movies
+          </h2>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+            {upcomings.results.map(
+              (movie) =>
+                movie.overview && <MovieCard key={movie.id} movie={movie} />
+            )}
+          </div>
+        </section>
       </main>
     </div>
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  const upcomings = await fetch(
+    `${process.env.BASE_URL}/api/upcoming?region=TR`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      upcomings,
+    },
+  };
+};
 export default Home;
